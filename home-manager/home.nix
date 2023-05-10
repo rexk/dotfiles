c59915cd9e1dcdfe204d0a8510acd6f4d59d4cf5
Myung Sun Kim <myungsunrex@gmail.com>
2023-05-10 10:36:06 -0700

    Allow impure base shell configuration (#3)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,5 +7,35 @@ in
   home.stateVersion = "22.11";
 
   programs.direnv.enable = true;
+
+  programs.bash = {
+    enable = true;
+    initExtra = "
+if [ -f $HOME/.config/bash/.bashrc ];
+then
+  source $HOME/.config/bash/.bashrc
+fi
+";
+  };
+
+  programs.fish = {
+    enable = true;
+    shellInit = "
+if test -f $HOME/.config/fish/config.base.fish
+  source $HOME/.config/fish/config/base.fish
+end
+";
+  };
+
+  programs.zsh = {
+    enable = true;
+    initExtra = "
+if [ -f $HOME/.config/zsh/.zshrc ];
+then
+  source $HOME/.config/zsh/.zshrc
+fi
+";
+  };
 }
+
 
